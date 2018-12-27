d5ca7e13677337f4f394fea38585bd6aa91049d6
Beatrice Mendoza <mendoza@nyu.edu>
2018-12-27 22:01:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,36 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
 
+  def self.find_by_name(name)
+    self.all.find { |song| song.name == name }
+  end
+  
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self
+    else
+      self.create_by_name(name)
+      self
+    end
+  end
 end
