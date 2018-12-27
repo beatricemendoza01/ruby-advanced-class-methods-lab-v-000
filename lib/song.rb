4680ed056614523ba753a212899f133289b92fbb
Beatrice Mendoza <mendoza@nyu.edu>
2018-12-27 22:12:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,6 +34,10 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    return self.find_by_name(name)
+    if self.find_by_name(name)
+      return self
+    else
+      return self.create_by_name(name)
+    end
   end
 end
